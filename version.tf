55313c26eff8cd34554960226a3fca410b58728e
Sri Harsha Vathsavayi <sriharsha1250@gmail.com>
2020-04-28 16:17:38 +0300

    Create version.tf

---
+++ version.tf
@@ -0,0 +1,21 @@
+variable "version" {
+  type = "string"
+  default = "0.1-SNAPSHOT"
+}
+
+resource "aws_elastic_beanstalk_application_version" "default" {
+  name        = "elastic-beanstalk-ndw-${var.version}"
+  application = "${aws_elastic_beanstalk_application.ndw-test.name}"
+  description = "application version created by terraform"
+  bucket      = "${data.aws_s3_bucket.ndw-test.id}"
+  key         = "${data.aws_s3_bucket_object.application-jar.key}"
+}
+
+data "aws_s3_bucket" "ndw-test" {
+  bucket = "ndw-test"
+}
+
+data "aws_s3_bucket_object" "application-jar" {
+  bucket = "${data.aws_s3_bucket.ndw-test.id}"
+  key    = "ndw-test/${var.version}/ndw-${var.version}.jar"
+}
