33593a1d68dd922a99793a7c2becef8152e7add0
Sri Harsha Vathsavayi <sriharsha1250@gmail.com>
2020-05-27 12:29:55 +0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "aws_command" {
-  value = module.java-webapp.ebs_update_environment_command
+  value = module.dev-webapp.ebs_update_environment_command
   description = "The command to deploy a version of app to environment"
 }
